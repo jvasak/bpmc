38d411c5bf0a2653f37fec277736ac6ada6a42d7
vsacker <anon.sacker@gmail.com>
2009-06-05 07:17:17 -0400

    More hacks at OOP for league
    Still working out some wierdness with overridden
    methods not properly calling the subclass version,
    but overall structure for league/conference/division/
    team setup is coming together.

--- league.py
+++ league.py
@@ -2,12 +2,10 @@
 
 class TeamGroup:
     """Abstract base class for organizing teams"""
-    __child  = []
-    __parent = None
-    __name   = ''
-
     def __init__(self, name):
-        self.__name = name
+        self.__name   = name
+        self.__parent = None
+        self.__child  = []
 
     def addChild(self, child):
         self.__child.append(child)
@@ -16,14 +14,21 @@ class TeamGroup:
     def setParent(self, parent):
         self.__parent = parent
 
-    def listChildren(self):
-        print self.__name
+    def listChildren(self, pre='', indent='  ', recurse=1):
+        print pre + self.__name
         for child in self.__child:
-            print "-> %s" % (child.getName())
-        print
+            if recurse:
+                child.listChildren(pre=pre+indent, indent=indent)
+            else:
+                print pre + indent + child.getName()
+
+    def getChildren(self):
+        return self.__child
 
     def getName(self):
         return self.__name
+
+
 
 
 class League(TeamGroup):
@@ -31,9 +36,29 @@ class League(TeamGroup):
     
     def __init__(self, name='NFL'):
         TeamGroup.__init__(self, name)
+
+
     
 
 class Conference(TeamGroup):
     """Simple class not much more than a named list of divisions"""
     pass
+
+
+
+class Division(TeamGroup):
+    pass
+
+
+
+class Team(TeamGroup):
+    """Group with no children is a team"""
+
+    def __init__(self, name, abbr):
+        TeamGroup.__init__(self, name);
+        self.__abbr = abbr;
+
+    def getName(self):
+        print 'YIKES'
+        return TeamGroup.getName(self) + ' (' + self.__abbr + ')'
 
--- nfl_sim.py
+++ nfl_sim.py
@@ -5,16 +5,38 @@ import sys
 
 from league import League
 from league import Conference
+from league import Division
+from league import Team
 
 if __name__ == "__main__":
-    nfl = League('NFL')
+    nfl = League()
     
     afc = Conference('AFC')
     nfc = Conference('NFC')
     
     nfl.addChild(afc)
     nfl.addChild(nfc)
+
+    nfce = Division('East')
+    nfcc = Division('Central')
+    nfcn = Division('North')
+    nfcw = Division('West')
+
+    nfc.addChild(nfce)
+    nfc.addChild(nfcc)
+    nfc.addChild(nfcn)
+    nfc.addChild(nfcw)
+
+    nfce.addChild(Team('Dallas Cowboys',       'DAL'))
+    nfce.addChild(Team('New York Giants',      'NYG'))
+    nfce.addChild(Team('Philladelphia Eagles', 'PHI'))
+    nfce.addChild(Team('Washington Redskins',  'WAS'))
+
+    teams = nfce.getChildren()
+    for team in teams:
+        team.getName()
+
     nfl.listChildren()
-    
+
     sys.exit(0)
     
