994a1fef3a65132b098f902807e43fd6360f4a6a
vsacker <anon.sacker@gmail.com>
2009-09-01 21:22:52 -0400

    Clean up tie break strategy
    Break out tie break step to support 3- (or more) way ties and re-use
    between division and conference tie breaks.

--- nfl.py
+++ nfl.py
@@ -2,6 +2,7 @@
 
 import logging
 import csv
+import random
 
 from league import League
 from league import Conference
@@ -9,10 +10,8 @@ from league import Division
 from league import Team
 
 
-
-def rankTeams(div):
+def rankTeams(teams, div=True):
     ranked = []
-    teams  = div.getTeams()
     
     # basic sort by win pct
     for team in teams:
@@ -27,71 +26,87 @@ def rankTeams(div):
             nextTeam.append(teams.pop(0))
             
         if len(nextTeam) > 1:
-            nextTeam = breakTie(nextTeam)
+            if div:
+                nextTeam = breakDivTie(nextTeam)
+            else:
+                nextTeam = breakConfTie(nextTeam)
             
         ranked.extend(nextTeam)
         nextTeam = []
             
     return ranked
 
-def breakTie(teams):
-    if len(teams) > 2:
-        logging.critical(" ########### Three- (or more) way ties not implemented ########### ")
-        return teams
+
+def tieBreakStep(teams, opps, callback, msg):
+    logging.info("Tie break between " + str(len(teams)) + " using " + msg)
+
+    for team in teams:
+        team.setWinPctOpponents(opps)
+    teams.sort(key=Team.getWinPct, reverse=True)
+
+    brkIdx = -1
+    wp = teams[0].getWinPct()
+    for i in range(1, len(teams)):
+        if teams[i].getWinPct() == wp:
+            continue
+        else:
+            logging.debug("Break off top " + str(i) + " teams using " + msg)
+            brkIdx = i
+            break
     
-    teams[0].setWinPctOpponents(teams)
-    teams[1].setWinPctOpponents(teams)
-    if teams[0].getWinPct() > teams[1].getWinPct():
-        logging.info(teams[0].getAbbr() + " better h2h")
+    if brkIdx == -1:
+        return (False, teams)
+
+    elif brkIdx == 1:
+        teams[1:] = callback(teams[1:])
+        return (True, teams)
+
+    teams[:brkIdx] = callback(teams[:brkIdx])
+    teams[brkIdx:] = callback(teams[brkIdx:])
+    return (True, teams)
+    
+
+def breakDivTie(teams):
+    if len(teams) == 1:
         return teams
-    elif teams[0].getWinPct() < teams[1].getWinPct():
-        logging.info(teams[1].getAbbr() + " better h2h (r)")
-        teams.reverse()
+
+    (finished, teams) = tieBreakStep(teams, teams, breakDivTie, "h2h")
+
+    if finished:
         return teams
 
     div = teams[0].getParent()
     divTeams = div.getTeams()
+    (finished, teams) = tieBreakStep(teams, divTeams, breakDivTie, "div")
 
-    teams[0].setWinPctOpponents(divTeams)
-    teams[1].setWinPctOpponents(divTeams)
-    if teams[0].getWinPct() > teams[1].getWinPct():
-        logging.info(teams[0].getAbbr() + " better in div")
+    if finished:
         return teams
-    elif teams[0].getWinPct() < teams[1].getWinPct():
-        logging.info(teams[1].getAbbr() + " better in div (r)")
-        teams.reverse()
-        return teams
-    
+
     commonOpps = set(teams[0].getOpponents())
     commonOpps.intersection(set(teams[1].getOpponents()))
+    (finished, teams) = tieBreakStep(teams, commonOpps, breakDivTie, "common")
     
-    teams[0].setWinPctOpponents(commonOpps)
-    teams[1].setWinPctOpponents(commonOpps)
-    if teams[0].getWinPct() > teams[1].getWinPct():
-        logging.info(teams[0].getAbbr() + " better in common")
+    if finished:
         return teams
-    elif teams[0].getWinPct() < teams[1].getWinPct():
-        logging.info(teams[1].getAbbr() + " better in common (r)")
-        teams.reverse()
-        return teams
-    
+
     conf = div.getParent()
     confTeams = conf.getTeams()
+    (finished, teams) = tieBreakStep(teams, confTeams, breakDivTie, "conf")
     
-    teams[0].setWinPctOpponents(confTeams)
-    teams[1].setWinPctOpponents(confTeams)
-    if teams[0].getWinPct() > teams[1].getWinPct():
-        logging.info(teams[0].getAbbr() + " better in conf")
-        return teams
-    elif teams[0].getWinPct() < teams[1].getWinPct():
-        logging.info(teams[1].getAbbr() + " better in conf (r)")
-        teams.reverse()
+    if finished:
         return teams
     
-    logging.critical("Still tied after conference games")
+    coin = random.randint(0, len(teams))
+    logging.info(teams[coin].getAbbr() + " by coin flip")
+    tmp         = teams[0]
+    teams[0]    = teams[coin]
+    teams[coin] = tmp
+    teams[1:]   = breakDivTie(teams[1:])
 
     return teams
 
+def breakConfTie(teams):
+    return teams
 
 class NFL:
     def __init__(self):
@@ -193,6 +208,7 @@ class NFL:
         return True
     
 
+
     def simulateSeason(self):
         self.__league.simulateRegularSeason()
         self.__genRegularSeasonStandings()
@@ -203,12 +219,16 @@ class NFL:
         confs = self.__league.getChildren()
         for cname in confs:
             conf = confs[cname]
+            leaders = []
             logging.info(conf.getName())
             divs = conf.getChildren()
             for dname in divs:
                 div = divs[dname]
                 logging.info(div.getName())
-                ranked = rankTeams(div)
+                ranked = rankTeams(div.getTeams())
                 for team in ranked:
                     logging.info(team.getName())
-
+                leaders.append(ranked.pop(0))
+            slotted = rankTeams(leaders, div=False)
+            for i in range(0,len(slotted)):
+                print ("%d: %s") % (i+1, slotted[i].getName())
